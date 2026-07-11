341bdaff7b6b64a2c5b40f57caa2aef355ac8d69
Prince <prince240102@gmail.com>
2026-07-11 11:57:19 +0530

    chore: checkpoint interrupted workflow work

--- desktop/src-tauri/src/main.rs
+++
@@ -1,6 +0,0 @@
-// Prevents an extra console window from popping up on Windows in release builds.
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-fn main() {
-    watchparty_desktop_lib::run();
-}
